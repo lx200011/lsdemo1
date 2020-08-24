3cacf9a65f593cff066380eb42235f628211657b
zr <247076126@qq.com>
2020-08-24 20:55:58 +0800

    update

--- day8/Zquery.js
+++ day8/Zquery.js
@@ -1,8 +1,4 @@
-
-
-
-~function (global) {
-
+~(function (global) {
   function Zquery(select) {
     this.select = [];
 
@@ -10,39 +6,55 @@
       this.select = document.querySelectorAll(select);
     } else if (select instanceof HTMLElement) {
       this.select = [select];
+    } else if (select instanceof Array) {
+      this.select = select;
     }
-
   }
 
   // new 完之后 prototype >>  __proto_
   Zquery.prototype.text = function (textString) {
-    console.log('', this.select);
+    console.log("", this.select);
     this.select.forEach(function (item) {
       item.innerText = textString;
-    })
+    });
 
     return this;
-  }
+  };
 
   // new 完之后 prototype >>  __proto_
   Zquery.prototype.addClass = function (classItem) {
     this.select.forEach(function (item) {
       item.classList.add(classItem);
-    })
+    });
     return this;
-  }
+  };
 
+  //添加事件
   Zquery.prototype.on = function (eventTag, callBack) {
     this.select.forEach(function (item) {
-      item.addEventListener(eventTag, callBack)
-    })
+      item.addEventListener(eventTag, callBack);
+    });
 
     return this;
-  }
+  };
+
+  //添加事件
+  Zquery.prototype.click = function (callBack) {
+    //new Zquery(this.select).on("click",callBack)
+    return this.on("click", callBack);
+  };
+
+  //取消监听事件
+  Zquery.prototype.off = function (eventTag, callBack) {
+    this.select.forEach(function (item) {
+      item.removeEventListener(eventTag, callBack);
+    });
+    return this;
+  };
 
   Zquery.prototype.eq = function (index) {
     return new Zquery(this.select[index]);
-  }
+  };
 
   Zquery.prototype.index = function () {
     let _that = this;
@@ -53,22 +65,58 @@
       }
     }
     return -1;
-  }
+  };
+
+  //获取dom上的属性
+  Zquery.prototype.attr = function (param,value) {
+    if(value == null){
+      return this.select[0].getAttribute(param);
+    }else{
+      return this.select[0].setAttribute(param,value);
+    }
+    
+  };
+
+  //判断是否包含传入的class名字如果包含返回true
+
+  Zquery.prototype.hasClass = function (className) {
+    //forEach()
+    //findIndex
+    //filter
+   
+    return this.attr("class")
+      .split(" ")
+      .findIndex(function (itme, index) {
+        if (itme == className) {
+          return true;
+        } else {
+          return false;
+        }
+      })  > 0 ? true : false
+  };
+
+  //判断是否包含传入的class名字如果包含返回true
+  Zquery.prototype.removeClass = function (className) {
+    this.select.forEach(function (item) {
+      item.classList.remove(className);
+    });
+    return this;
+  };
 
   Zquery.prototype.parent = function () {
+    return new Zquery(this.select[0].parentElement);
+  };
 
-    return new Zquery(this.select[0].parentElement)
-  }
-
+  //找到兄弟节点,不包含自己
   Zquery.prototype.sibling = function () {
     console.log(this.select[0].Sibling);
-    return new Zquery()
-  }
+    return new Zquery();
+  };
 
   global.z$ = function (select) {
     return new Zquery(select);
-  }
-}(window);
+  };
+})(window);
 
 // ~
 // !
@@ -76,6 +124,5 @@
 // (function (global) {
 
 //   console.log(111111111);
-
 
 // })(window);
